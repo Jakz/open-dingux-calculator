94e4d1beb15f4c014d7985843c8f80481e912761
Jack <jack@pixbits.com>
2019-11-27 21:49:26 +0100

    added SDL2_TTF with some test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
 
 #include <cstdint>
 
@@ -15,6 +17,7 @@ private:
   SDL_Renderer* renderer;
   
   SDL_Texture* textureUI;
+  TTF_Font* font;
 
   bool willQuit;
   u32 ticks;
@@ -29,11 +32,14 @@ public:
   bool init();
   void deinit();
   void capFPS();
+  bool loadData();
+  
   void loop();
-
   void render();
   void handleEvents();
 
+
+  void blit(SDL_Texture* texture, int dx, int dy);
 };
 
 bool SDL::init()
@@ -41,9 +47,16 @@ bool SDL::init()
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     return false;
 
+  if (IMG_Init(IMG_INIT_PNG) == 0)
+    return false;
+
+  TTF_Init();
+
   // SDL_WINDOW_FULLSCREEN
   window = SDL_CreateWindow("ODCalc v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 200, SDL_WINDOW_OPENGL);
   renderer = SDL_CreateRenderer(window, -1, 0);
+
+  SDL_SetRenderDrawColor(renderer, 236, 232, 228, 255);
 }
 
 void SDL::loop()
@@ -56,8 +69,6 @@ void SDL::loop()
     capFPS();
   }
 }
-
-char titleBuffer[64];
 
 void SDL::capFPS()
 {
@@ -79,11 +90,16 @@ void SDL::capFPS()
 
 void SDL::deinit()
 {
+  TTF_CloseFont(font);
+
+  TTF_Quit();
+  IMG_Quit();
+  
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
   SDL_DestroyTexture(textureUI);
-
+  
   SDL_Quit();
 }
 
@@ -105,9 +121,58 @@ void SDL::handleEvents()
   }
 }
 
+void SDL::blit(SDL_Texture* texture, int dx, int dy)
+{
+  u32 dummy;
+  int dummy2;
+
+  SDL_Rect from = { 0, 0, 0, 0 };
+  SDL_Rect to = { dx, dy, 0, 0 };
+
+  SDL_QueryTexture(texture, &dummy, &dummy2, &from.w, &from.h);
+
+  to.w = from.w;
+  to.h = from.h;
+
+  SDL_RenderCopy(renderer, texture, &from, &to);
+}
+
+bool SDL::loadData()
+{
+  SDL_Surface* surfaceUI = IMG_Load("../../../data/ui.png");
+
+  if (!surfaceUI)
+    return false;
+
+  textureUI = SDL_CreateTextureFromSurface(renderer, surfaceUI);
+  SDL_FreeSurface(surfaceUI);
+
+  font = TTF_OpenFont("../../../data/FreeSans.ttf", 10);
+
+  return true;
+}
+
 void SDL::render()
 {
+  SDL_Rect rect = { 0, 0, 16, 16 };
+  SDL_Rect rect2 = { 10, 10, 16, 16 };
 
+  SDL_RenderClear(renderer);
+
+  SDL_RenderCopy(renderer, textureUI, &rect, &rect2);
+
+
+  SDL_Color color = { 0, 0, 0 };
+  SDL_Surface* surface = TTF_RenderText_Blended(font, "This is a nice test.", color);
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+  blit(texture, 10, 10);
+
+  SDL_DestroyTexture(texture);
+  SDL_FreeSurface(surface);
+
+
+
+  SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char* argv[])
@@ -115,6 +180,7 @@ int main(int argc, char* argv[])
   SDL sdl;
 
   sdl.init();
+  sdl.loadData();
   sdl.loop();
   sdl.deinit();
 
